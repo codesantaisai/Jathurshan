ad59177554425183b4726a66bd817e15f4022876
codesantaisai <jathurshan@protonmail.com>
2024-05-06 18:23:35 +0530

    contact done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import About from './components/About'
+import Contact from './components/Contact'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
@@ -13,6 +14,7 @@ function App() {
     <About/>
     <Projects/> 
     <Resume/>
+    <Contact/>
     </>
   )
 }
---
+++ src/components/Contact.tsx
@@ -0,0 +1,15 @@
+
+function Contact() {
+  return (
+    <section className="flex  flex-col bg-primary px-5 py-32">
+        <div className="flex flex-col items-center text-white">
+            <h1 className="text-4xl border-b-4 border-secondary mb-5 w-[135px] font-bold">Contact</h1>
+            <p className="pb-5 text-xl">If you want to discuss more in details,please contact me </p>
+            <p className="py-2 text-xl "><span className="font-bold">Email: </span>Jathurshan@protonmail.com</p>
+            <p className="py-2 text-xl"><span className="font-bold">Phone: </span>+946443471</p>
+        </div>
+    </section>
+  )
+}
+
+export default Contact
